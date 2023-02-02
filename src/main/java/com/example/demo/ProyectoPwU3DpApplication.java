cdcd1548c25d5b479bdc40dcf2398a58adcc9969
Dennisyaj <dennispilco@gmail.com>
2023-02-01 19:45:49 -0500

    creacion proyecto

---
+++ src/main/java/com/example/demo/ProyectoPwU3DpApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPwU3DpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPwU3DpApplication.class, args);
+	}
+
+}
