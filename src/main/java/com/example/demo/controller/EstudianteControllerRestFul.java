457f73ea202b0786d53992fb53bdabe76051ac6c
Dennisyaj <dennispilco@gmail.com>
2023-02-04 10:54:00 -0500

    taller 27

--- src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,21 +15,30 @@ import com.example.demo.service.IEstudianteService;
 @RestController
 @RequestMapping("/estudiantes")
 public class EstudianteControllerRestFul {
-	
-	@Autowired
-	IEstudianteService iEstudianteService ;
 
+	@Autowired
+	IEstudianteService iEstudianteService;
+
+	@PostMapping("")
 	public void registrarEstudiante(Estudiante estudiante) {
 	}
 
-	public void actualizarEstudiante(Estudiante estudiante) {
+	@PutMapping(path = "/{id}")
+	public void actualizarEstudiante(@PathVariable("id") Integer id, Estudiante estudiante) {
+	this.iEstudianteService.actualizarEstudiante(estudiante);
 	}
 
-	@GetMapping(path = "/buscar/{id}")
+	@PutMapping
+	public void actualizartodosEstudiante(Estudiante estudiante) {
+	}
+
+	@GetMapping(path = "/{id}")
 	public Estudiante encontrar(@PathVariable("id") Integer id) {
 		return this.iEstudianteService.encontrar(id);
 	}
 
-	public void borrar(Integer id) {
+	@DeleteMapping(path = "/{id}")
+	public void borrar(@PathVariable("id") Integer id) {
+		this.iEstudianteService.borrar(id);
 	}
 }
