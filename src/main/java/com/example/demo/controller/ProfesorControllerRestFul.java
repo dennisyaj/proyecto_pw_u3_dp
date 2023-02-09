6ce7bc5842ac53e68e8b4d05be6aea90edd28641
dennisyaj <dennispilco@gmail.com>
2023-02-09 13:40:12 -0500

    Tarea 15

--- src/main/java/com/example/demo/controller/ProfesorControllerRestFul.java
+++ src/main/java/com/example/demo/controller/ProfesorControllerRestFul.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +35,17 @@ public class ProfesorControllerRestFul {
 	@DeleteMapping(path = "/{id}")
 	public void eliminar(@PathVariable("id") Integer id) {
 		this.iProfesorService.borrar(id);
+	}
+
+	@PostMapping
+	public void registrarProfesor(@RequestBody Profesor profesor) {
+		this.iProfesorService.registrarProfesor(profesor);
+	}
+
+	@PutMapping(path = "/{id}")
+	public void actualizarProfesor(@PathVariable("id") Integer id, @RequestBody Profesor profesor) {
+		profesor.setId(id);
+		this.iProfesorService.actualizarProfesor(profesor);
 	}
 
 }
